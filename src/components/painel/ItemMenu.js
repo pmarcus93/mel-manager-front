c5262519844ec4523dff0278bc969be7e2fe0efd
Lucas <lucasmg37@gmail.com>
2019-10-18 09:23:03 -0300

    criando-projeto-base - Item de Menu Side Bar

---
+++ src/components/painel/ItemMenu.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import './ItemMenu.scss';
+
+export default function ItemMenu(props) {
+
+    return (
+        <div
+            onClick={props.closeMenu ? () => {
+                props.actionCloseMenu();
+                props.menu.action();
+
+            } : props.menu.action}
+
+            className="item-bar">
+            <span className="icon-bar"> <FontAwesomeIcon icon={props.menu.icon}/></span>
+            <span> {props.menu.text} </span>
+        </div>
+    );
+
+}
