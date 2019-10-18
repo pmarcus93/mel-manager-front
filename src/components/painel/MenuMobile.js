b5539c1d53c6b636ed72903997ada951d3e88b30
Lucas <lucasmg37@gmail.com>
2019-10-18 09:34:22 -0300

    criando-projeto-base - Implementando Menu Mobile

---
+++ src/components/painel/MenuMobile.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Logo from "../../assets/img/logo.png";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+import './MenuMobile.scss';
+import ItemMenu from "./ItemMenu";
+import {faArrowLeft} from "@fortawesome/free-solid-svg-icons";
+
+export default function MenuMobile(props) {
+
+    return (
+        <div className={!props.controlMenu ? 'sidebar-mobile hide-mobile' : 'sidebar-mobile'}>
+
+            <div className='menu-nav-bar-mobile'>
+                <span onClick={props.setControlMenu} className='arrow-back-menu-mobile'>
+                    <FontAwesomeIcon icon={faArrowLeft} />
+                </span>
+
+                <div className="logo-bar">
+                    <img src={Logo} alt='Logo MEL'/>
+                </div>
+            </div>
+
+
+            <div className="perfil-info-bar">
+
+                <div className="perfil-avatar-bar">
+                    <img src="http://www.carderator.com/assets/avatar_placeholder_small.png" alt='Perfil do usuário'/>
+                </div>
+
+                <div className="perfil-info">
+                    <strong>Lucas Jr</strong>
+                    <p>Usuário</p>
+                </div>
+
+            </div>
+
+            <div className="items-bar">
+
+                {props.itensMenu.map(menu => (
+                    <ItemMenu menu={menu} closeMenu={true} actionCloseMenu={props.setControlMenu} />
+                ))}
+
+            </div>
+        </div>
+    )
+
+}
